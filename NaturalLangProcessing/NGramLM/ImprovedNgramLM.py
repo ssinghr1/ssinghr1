c2778b3e0e33f81b9f9d6319baa889772b8a47b6
ssinghr1 <ssinghr1@jh.edu>
2022-11-07 00:59:39 -0500

    fixing errors

---
+++ NaturalLangProcessing/NGramLM/ImprovedNgramLM.py
@@ -0,0 +1,71 @@
+import NgramLM
+
+class ImprovedNgramLM(NgramLM.NgramLM):
+    """An improved version of n-gram language model with OOV handling.
+    This class inherits all bahaviors from previous defined `NgramLM`.
+    Please be careful with the implementations here as you have to be consistent
+    with all interfaces.
+    
+    Note: you could override more methods provided by the base class, but you have 
+    to maintain their ability of handling the same types of inputs and outputs.
+    """
+
+    def __init__(self, n: int):
+        super(ImprovedNgramLM, self).__init__(n=n)
+           
+        self.a: int = 0.2 #add-a smoothing constant
+        #self.a = .0016191 #add-a smoothing constant with lowest perplexity from testing. 
+
+    @overrides
+    def next_word_candidates(self, context: Tuple[str]) -> List[str]:
+        """Generates a list of tokens based on the given context, which would be
+        later used as candidates for the next word prediction.
+        
+        Note: in this overriden version, you should deal with the OOV words.
+        
+        Parameters
+        ----------
+        context : Tuple[str]
+            A tuple of words describing the context for the next word.
+            
+        Returns
+        -------
+        words : List[str]
+            A list of candidate tokens for the next word.
+        """
+        candidates = self.contexts[context]
+        candidates.add('<UNK>')
+        return list(candidates)
+        ...
+
+    @overrides
+    def word_prob(self, context: Tuple[str], word: str) -> float:
+        """Returns the probability of a word given a context. The context is a
+        string of words, with length n-1. 
+        
+        Note: in this overriden version, you should deal with the OOV words.
+        
+        Parameters
+        ----------
+        context : Tuple[str]
+            A tuple of words describing the context for the next word.
+        word : str
+            The next word that the probability is computed for.
+            
+        Returns
+        -------
+        prob : float
+            The estimated probability of the next word given the context.
+        """
+
+        complete_ngram = context + (word,)
+        if complete_ngram in self.ngrams:
+          counts_word = float(self.ngrams[complete_ngram])
+        else:
+          counts_word = float(self.ngrams[context + ('<UNK>',)])
+
+        counts_context = float(self.ngram_contexts[context])
+        prob = (counts_word+self.a)/(counts_context+self.a*len(self.vocab))
+
+        return prob
+        ...
